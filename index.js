5889995025baabf01ca9bb385f52bc681759fad7
balutbomber <balutbomber@gmail.com>
2024-05-29 11:10:40 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const LaunchRequestHandler = require( './LaunchRequestHandler' );
+// const StartTriviaHandler = require( './StartTriviaHandler' );
+// const RepeatIntentHandler = require('./RepeatIntentHandler');
+// const GetAnswerIntentHandler = require( './GetAnswerIntentHandler' );
+const HelloWorldIntentHandler = require('./HelloWorldIntentHandler');
+// The help intent handler is setup to be invoked for fallback intent also
+const HelpIntentHandler = require('./HelpIntentHandler');
+const CancelAndStopIntentHandler = require('./CancelAndStopIntentHandler');
+const SessionEndedRequestHandler = require('./SessionEndedRequestHandler');
+const IntentReflectorHandler = require('./IntentReflectorHandler');
+
+const AWS = require('aws-sdk');
+const Alexa = require('ask-sdk-core');
+// const dynamoAdapter = require('ask-sdk-dynamodb-persistence-adapter');
+
+//const interceptors = require( './interceptors' );
+// const { 
+//     LoadDataInterceptor, 
+//     LoggingRequestInterceptor,
+//     SaveDataInterceptor,
+//     LoggingResponseInterceptor,
+// } = interceptors;
+
+const ErrorHandler = require('./ErrorHandler');
+
+// order in which handlers are called matters
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+//        StartTriviaHandler,
+//        RepeatIntentHandler,
+//        GetAnswerIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler
+    )
+    // .addRequestInterceptors(
+    //     LoadDataInterceptor,
+    //     LoggingRequestInterceptor,
+    // )
+    // .addResponseInterceptors(
+    //     SaveDataInterceptor,
+    //     LoggingResponseInterceptor,
+    // )
+    .addErrorHandlers(
+        ErrorHandler
+    )
+    // .withPersistenceAdapter(
+    //     new dynamoAdapter.DynamoDbPersistenceAdapter({
+    //         tableName: process.env.DYNAMODB_TABLE,
+    //         createTable: true,
+    //         dynamoDBClient: new AWS.DynamoDB({
+    //             apiVersion: 'latest', region: process.env.DYNAMODB_REGION
+    //         })
+    //     })
+    // )
+    .withCustomUserAgent('sample/alexa-named-alarm/v1.2')
+    .lambda();
